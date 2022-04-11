cc20b20fa90b18243b0372f43cc4fd2c30756ed1
ImkyeongJeong <imkyeongjeong@gmail.com>
2022-04-12 00:16:22 +0900

    오렌지, 영수증 숙제

---
+++ HelloWorld/src/co/edu/variable/HomeWork1.java
@@ -0,0 +1,33 @@
+package co.edu.variable;
+
+import java.util.Scanner;
+
+public class HomeWork1 {
+
+	public static void main(String[] args) {
+		
+		//오렌지 127개 10개씩 담을 수 있는 상자. 5개씩 담을 수 있는 상자. 몇개 필요한지 출력 2개가 남을 경우 5개짜리 상자로
+		
+		int orange;
+		int box10;
+		int box5;
+		int result;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("오렌지 개수를 입력하시오.");
+		orange = sc.nextInt();
+		
+		box10 = orange/10;
+		result = orange%10;
+		box5 = result/5;
+		result = orange%5;
+		
+		if(result>1) {
+			box5 ++;
+		}
+		
+		System.out.printf("오렌지 %d개는 10개씩 담을 수 있는 상자 %d개, 5개씩 담을 수 있는 상자 %d개가 필요합니다.", orange, box10, box5);
+	}
+
+}
---
+++ HelloWorld/src/co/edu/variable/HomeWork2.java
@@ -0,0 +1,36 @@
+package co.edu.variable;
+
+import java.util.Scanner;
+
+public class HomeWork2 {
+
+	public static void main(String[] args) {
+		
+		// 영수증) 물건의 가격과 현금 입력받음
+		// 현금 10000원
+		// 물건가격 7500원 (부가세까지 포함된 가격)
+		// 부가세 750원
+		// 잔돈 2500원
+		
+		int cash;
+		int price;
+		int vat;
+		int result;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("현금 입력");
+		cash = sc.nextInt();
+		System.out.println("물건가격 입력");
+		price = sc.nextInt();
+
+		//부가세 구하는 공식
+		vat = price - (int)(price/1.1);
+		
+		System.out.println("[현금] " + cash + "원");
+		System.out.println("[물건가격] " + price + "원");
+		System.out.println("[부가세]" + vat + "원");
+		System.out.println("[잔돈]" + (cash - price) + "원");
+	}
+
+}
