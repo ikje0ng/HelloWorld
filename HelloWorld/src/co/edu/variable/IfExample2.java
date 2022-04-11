0f268b306af387d101fc756aef61d7b0767cba35
admin <admin@yd2-07>
2022-04-11 14:23:10 +0900

    com/pu

---
+++ HelloWorld/src/co/edu/variable/IfExample2.java
@@ -0,0 +1,16 @@
+package co.edu.variable;
+
+public class IfExample2 {
+	public static void main(String[] args) {
+		int num1, num2;
+		
+		num1 = 100;
+		num2 = 200;
+		
+		if(num1 > num2) {
+			System.out.println("큰수는: " + num1);
+		} else {
+			System.out.println("큰수는: " + num2);
+		}
+	}
+}
